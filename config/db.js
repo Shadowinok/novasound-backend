41e35b1d8a209517911b8255b721ba0476f39190
Shadowinok <shadowinok@gmail.com>
2026-03-22 10:31:11 +1000

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -4,6 +4,12 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
+    try {
+      const TrackReport = require('../models/TrackReport');
+      await TrackReport.updateMany({ reportType: { $exists: false } }, { $set: { reportType: 'content' } });
+    } catch (e) {
+      console.warn('TrackReport migration:', e.message);
+    }
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
